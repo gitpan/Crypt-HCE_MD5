b601c545367a11797a1ed94a810fba3341c9131f
Eric Estabrooks <eric@urbanrage.com>
1999-04-07 04:16:55 +0000

    Import of EESTABROO/Crypt-HCE_MD5-0.40 from CPAN.
    gitpan-cpan-distribution: Crypt-HCE_MD5
    gitpan-cpan-version:      0.40
    gitpan-cpan-path:         EESTABROO/Crypt-HCE_MD5-0.40.tar.gz
    gitpan-cpan-author:       EESTABROO
    gitpan-cpan-maturity:     released

--- HCE_MD5.pm
+++ HCE_MD5.pm
@@ -2,7 +2,7 @@
 # Crypt::HCE_MD5
 # implements one way hash chaining encryption using MD5
 #
-# $Id$
+# $Id: HCE_MD5.pm,v 1.2 1999/04/07 04:14:13 eric Exp $
 #
 
 package Crypt::HCE_MD5;
@@ -10,7 +10,7 @@ package Crypt::HCE_MD5;
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-use MD5;
+use Digest::MD5;
 use MIME::Base64;
 use Carp;
 
@@ -24,7 +24,7 @@ require AutoLoader;
 @EXPORT = qw(
 	
 );
-$VERSION = '0.03';
+$VERSION = '0.40';
 
 sub new {
     my $class = shift;
@@ -46,7 +46,7 @@ sub _new_key {
     my $self = shift;
     my ($rnd) = @_;
  
-    my $context = new MD5;
+    my $context = new Digest::MD5;
     $context->add($self->{SKEY}, $rnd);
     my $digest = $context->digest();
     my @e_block = unpack('C*', $digest);
@@ -136,8 +136,7 @@ Crypt::HCE_MD5 - Perl extension implementing one way hash chaining encryption us
 
   $mime_crypted = $hce_md5->hce_block_encode_mime("Encrypt and Base64 this information");
   $info = $hce_md5->hce_block_decode_mime($mime_crypted);
-  
- 
+
 =head1 DESCRIPTION
 
 This module implements a chaining block cipher using a one way hash.  This method of encryption is the same that is used by radius (RFC2138) and is also described in Applied Cryptography.
@@ -150,7 +149,7 @@ An example client and server are packaged as modules with this module.  They are
 
 =head1 AUTHOR
 
-Eric Estabrooks, estabroo@ispn.com
+Eric Estabrooks, eric@urbanrage.com
 
 =head1 SEE ALSO
 
